7241c60a885b0debb658149ce70295cd01e14f0f
odanoburu <bcclaro@gmail.com>
2017-09-26 15:23:02 -0300

    add contractions and remove mtoken handling

--- src/projeto/rec-conllu.lisp
+++ src/projeto/rec-conllu.lisp
@@ -23,82 +23,75 @@
 ;; in the end of the file there are examples of entity recognition and
 ;; count in conllu files.
 
-;; problem: when working with mtokens mixed with tokens, the indices
-;; returned by recognize-ents-in-sentences might not be the real
-;; indices.
-
 
 (ql:quickload :cl-conllu)
+(ql:quickload :split-sequence)
 (ql:quickload :alexandria)
 
 (load (compile-file #P"~/git/ed-2017-2/src/projeto/rec-entities.lisp"))
 
 ;;
-;; from sentences to list of strings
-(defun get-token-form (token)
-  (etypecase token
-    (cl-conllu:token (cl-conllu:token-form token))
-    (cl-conllu:mtoken (cl-conllu:mtoken-form token))))
+;; pre-process entity list
+(defparameter *decontractions* (copy-tree '(("dos" . ("de" "os"))
+                                            ("do" . ("de" "o"))
+                                            ("das" . ("de" "as"))
+                                            ("da" . ("de" "a"))
+                                            ("pelas" . ("por" "as"))
+                                            ("pelas" . ("por" "a"))
+                                            ("pelos" . ("por" "os"))
+                                            ("pelo" . ("por" "o"))
+                                            ("às" . ("a" "as"))
+                                            ("à" . ("a" "a"))
+                                            ("na" . ("em" "a"))
+                                            ("nas" . ("em" "as"))
+                                            ("nos" . ("em" "os"))
+                                            ("no" . ("em" "o"))
+                                            ("aos" . ("a" "os"))
+                                            ("ao" . ("a" "o")))))
 
-(defun aux-construct-token-list (tokens start end
-				 &key token-list mtoken)
-  (when (endp tokens) (return-from aux-construct-token-list
-			(values nil token-list)))
-;; because an mtoken can be the last one in a sentence.
-  (let* ((token (first tokens))
-	(token-id (cl-conllu:token-id token))
-	(rest-tokens (rest tokens)))
-    (cond ((< token-id start)
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list (cons token
-						       token-list)
-				     :mtoken mtoken))
-	  ((= token-id start)
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list (cons mtoken
-						       token-list)))
-	  ((and (> token-id start) (<= token-id end))
-	   (aux-construct-token-list rest-tokens start end
-				     :token-list token-list))
-	  ((> token-id end)
-	   (values tokens token-list)))))
+(defun decontract-names (name-tokens subs &optional decontracted)
+  (if (endp name-tokens)
+      decontracted
+      (let ((name (first name-tokens)))
+        (decontract-names (rest name-tokens) subs
+                          (append decontracted
+                                  (or (rest (assoc name subs
+                                                   :test #'equal))
+                                      (list name)))))))
 
-(defun construct-token-list (tokens mtokens &optional token-list)
-  "construct list of tokens, replacing tokens by their respective
-mtokens: (pt em o governo) -> (pt no governo)"
-  (if (endp mtokens)
-      (append (reverse token-list) tokens)
-      (let* ((mtoken (first mtokens))
-	     (start (cl-conllu:mtoken-start mtoken))
-	     (end (cl-conllu:mtoken-end mtoken)))
-	(multiple-value-bind (rest-tokens result-token-list)
-	    (aux-construct-token-list tokens start end :mtoken mtoken)
-	  (construct-token-list rest-tokens (rest mtokens)
-				(append result-token-list
-					token-list))))))
+(defun conllu-process-entity (string &optional
+                                            (subs *decontractions*)
+                                            (separator #\space))
+  (alexandria:mappend #'str-to-char
+                      (decontract-names
+                       (split-sequence:split-sequence separator
+                                                      (string-trim
+                                                       '(#\space #\tab)
+                                                       string))
+                       subs)))
 
+(defun conllu-process-entities (entities)
+  (process-entities entities :process-fn #'conllu-process-entity))
+
+;;
+;; process conllu sentences to list of list of chars
 (defun process-form (token)
-  (process-string (get-token-form token)))
-
-(defun cons-tokens-from-sentence (sentence)
-  "this pre-processes cl-conllu classes for input"
-  (construct-token-list (cl-conllu:sentence-tokens sentence)
-                        (cl-conllu:sentence-mtokens sentence)))
+  (str-to-char (cl-conllu:token-form token)))
 
 (defun cons-tokens-from-sentences (sentences)
-  (mapcar #'cons-tokens-from-sentence sentences))
-
-(defun forms-from-sentence (sentence)
-  (mapcar #'get-token-form sentence))
-
-(defun forms-from-sentences (sentences)
-  (mapcar #'forms-from-sentence sentences))
+  (mapcar #'cl-conllu:sentence-tokens sentences))
 
 (defun chars-from-sentence (sentence)
   (mapcar #'process-form sentence))
 
 (defun chars-from-sentences (sentences)
   (mapcar #'chars-from-sentence sentences))
+
+(defun forms-from-sentence (sentence-tokens)
+  (mapcar #'cl-conllu:token-form sentence-tokens))
+
+(defun forms-from-sentences (sentences)
+  (mapcar #'forms-from-sentence sentences))
 
 
 ;;
@@ -112,7 +105,7 @@ mtokens: (pt em o governo) -> (pt no governo)"
 
 (defun trie-from-entities (path)
   (let* ((raw-ents (read-entities path))
-        (ents (process-entities raw-ents))
+        (ents (conllu-process-entities raw-ents))
         (trie (start-trie ents)))
     (values trie ents)))
 
@@ -163,7 +156,7 @@ one file at time, which prevents stack overflow."
        (form-sents (forms-from-sentences token-sents))
        (char-sents (chars-from-sentences token-sents))
        (raw-ents (read-entities #p"entities.txt"))
-       (ents (process-entities raw-ents))
+       (ents (conllu-process-entities raw-ents))
        (trie (start-trie ents))
        (rec-entities (recognize-ents-in-sentences trie char-sents)))
   raw-sents
